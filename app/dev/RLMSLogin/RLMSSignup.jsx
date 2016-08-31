1278721e67f048331185d54d366881a502b76078
SCHWIECK\DeVonFire3 <DeVonFire3@gmail.com>
2016-08-31 13:02:30 -0400

    bootstrap sign up page

--- app/dev/RLMSLogin/RLMSSignup.jsx
+++ app/dev/RLMSLogin/RLMSSignup.jsx
@@ -13,8 +13,7 @@ class SignUpApp extends React.createClass({
   render(){
     return (
       <div>
-      <RegisterForm/>
-      <button type="button" onClick={this.ret}>Cancel</button>
+      <RegisterForm data={this.ret}/>
       </div>
     );
   }
@@ -23,24 +22,27 @@ class SignUpApp extends React.createClass({
 
 const RegisterForm = (props) =>{
   return (
-    <div>
+    <div id="registerDiv" className="row col-sm-8 col-sm-offset-2">
+    <span className="form-control text-center">
     <h1><p>Join RLMS!</p></h1>
     <h2>Sign Up</h2>
     
     <form action="/api/signup" method="post">
       <label>First Name: </label>
-      <input type="text" name="firstname" id="firstname"></input>
+      <input type="text" className="form-control col-sm-6" name="firstname" id="firstname"></input><br/>
       <label>Last Name: </label>
-      <input type="text" name="lastname" id="lastname"></input>
+      <input type="text" className="form-control col-sm-6" name="lastname" id="lastname"></input><br/>
       <label>Username: </label>
-      <input type="text" name="username" id="username"></input>
+      <input type="text" className="form-control col-sm-6" name="username" id="username"></input><br/>
       <label>Password: </label>
-      <input type="password" name="password" id="password"></input>
+      <input type="password" className="form-control col-sm-6" name="password" id="password"></input><br/>
       <label>Confirm Password:</label>
-      <input type="password" name="cPass" id="cPass"></input>
-      <button type="submit">Register</button>
+      <input type="password" className="form-control col-sm-6" name="cPass" id="cPass"></input><br/>
+      <button type="submit" className="btn btn-primary col-sm-2">Register</button>
+      <button type="button" onClick={props.data} className="btn btn-info col-sm-2 pull-right">Cancel</button>
     </form>
     
+    </span>
     </div>
   )
 }
