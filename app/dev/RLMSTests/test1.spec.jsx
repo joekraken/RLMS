97f2fb55b384037ecccf3da72dc79bfb9de23d49
Jordan <jordanclarke1@gmail.com>
2016-09-01 13:19:15 -0400

    fix test

--- app/dev/RLMSTests/test1.spec.jsx
+++ app/dev/RLMSTests/test1.spec.jsx
@@ -1,34 +1,3 @@
-//put file in /RLMSTests
-/*
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-
-import { mount, shallow } from 'enzyme';
-import Curriculum from '../RLMSCurriculum/index.jsx'
-import jasmine from 'jasmine-ajax'
-
-describe('make sure header appears', function () {
-  
-  it('should have lessons', ()=>{
-    const wrapper = mount(<Curriculum/>);
-    expect(1).toEqual(1);
-  });
-
-});
-
-
-describe('test ajax calls', function () {
-
-  
-  it('should render syllabus correctly', ()=>{
-    const wrapper = mount(<Curriculum/>);
-    expect(1).toEqual(1);
-
-  });
-
-});
-export default Tests
-*/
 
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
@@ -48,12 +17,11 @@ describe('make sure header appears', function () {
 
 
 describe('test ajax calls', function () {
-       const wrapper = mount(<Curriculum/>);
 
-  beforeEach(function() {
-       //const wrapper = mount(<Curriculum/>);
+  beforeEach(function() { 
+    const wrapper = mount(<Curriculum/>);
 
-      sessionStorage.setItem('batchName','0-0-Java');
+     sessionStorage.setItem('batchName','0-0-Java');
 
     });
   
@@ -63,19 +31,9 @@ describe('test ajax calls', function () {
        expect(wrapper.find('h3').length).toEqual(1);
        expect(wrapper.find('h3').text()).toEqual('Week 1: Core');
 
-    done(); // call this to finish off the it block
+    done(); 
   }, 5000);
-    //expect(wrapper.find('h3').length).toEqual(1);
-   
-
-    //var comp = <Curriculum />;
-    //var DOM = TestUtils.renderIntoDocument(component);
-    //var res = DOM.findRenderedComponentWithType(comp, <h3/> )
-
-    //expect(res).not.toBeUndefined();
-
-    //expect(wrapper.find('td').first().text()).toEqual('Java');
-    //expect(wrapper.find('td').get(1).text()).toEqual('test');
+    expect(wrapper.find('h1').length).toEqual(1);   
 
   });
 
